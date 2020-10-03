b4fe9afc64997f7756f2e37d72a0d430e4f56d79
Preston-Too <toopreston@gmail.com>
2020-10-03 12:17:07 +0300

    added date format and and a display message for day born and Akan name

--- js/script.js
+++ js/script.js
@@ -18,3 +18,25 @@ if (day <= 0 || day > 31 ){
     alert("Please enter a valid year")
 }
 
+var a = Math.floor((14 - month) / 12)
+var y = year - a
+var m = month + 12 * a - 2
+var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(year / 400) + Math.floor((31 * m )/ 12)) % 7;
+
+if (male.checked == true){
+    document.getElementById("message").innerHTML=("You were born on <span>" + daysOfTheWeek[d] +  "</span> and your Akan name is <span>" + maleNames[d]);
+    console.log(daysOfTheWeek[d]);
+    console.log(maleNames[d]);
+}
+else if (female.checked == true){
+    document.getElementById("message").innerHTML=("You were born on <span>" + daysOfTheWeek[d] +  " </span> and your Akan name is <span>" + femaleNames[d]);
+    console.log(daysOfTheWeek[d]);
+    console.log(femaleNames[d]);
+}
+else {
+    alert("Select gender")
+}
+var form = document.getElementById("myForm");{
+    function handleForm(event) { event.preventDefault(); } 
+    form.addEventListener('submit', handleForm); 
+}
